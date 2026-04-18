75f14b376a3bf7998679fd6a78cfff9408eb299a
elango <elangosubramani12@gmail.com>
2026-04-18 18:22:50 +0530

    missed test class

--- src/test/java/tests/secondtest.java
+++ src/test/java/tests/secondtest.java
@@ -1,9 +1,24 @@
 package tests;
 
+import java.util.List;
+
+import org.testng.annotations.Test;
+
 import base.baseTest;
+import pages.homePage;
 
 public class secondtest extends baseTest {
 
+    @Test
+    public void verifyMenuItems() {
+
+    homePage listElement = new homePage(driver);
+
+    List<String> items = listElement.elements();
+
+    for (String item : items) {
+        System.out.println(item);
+    }
     
-    
+}
 }
